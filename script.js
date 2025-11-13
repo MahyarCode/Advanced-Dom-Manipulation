60c16d6aadeaeefa0e8d6d48ec4e46162165efdc
Mahyar Mousavinia <mahyar.mousavi.1408@gmail.com>
2025-11-13 18:08:16 +0400

    Added only the cookie element to the DOM

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,21 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// TODO Create, insert and delete a Cookie element:
+
+// DESC Create
+const header = document.querySelector('.header'); // 1. Specify the Parent node
+const message = document.createElement('div'); // 2. Specify the ElementTagName
+message.innerHTML =
+  'We use cookie for better functionality and analytics. <button class="btn btn--close-cookie">Got it</button>'; // 3. Specify the innerHTML
+
+// DESC Insert
+header.prepend(message); // 4. insert it
+
+// DESC Delete
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  }); // 5. Delete it
